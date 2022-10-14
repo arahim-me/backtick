226ccf6cd3588935cfb0e0ffb2e373bb8164ed8f
unknown <rahim.skilledit@gmil.com>
2022-10-15 01:24:11 +0600

    Scroll Behevior fixed

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import { useState, React } from 'react';
 import './About.css';
 import Ratings from '../../Data/rating.json';
+import ScrollTrigger from 'react-scroll-trigger';
+import CountUp from 'react-countup';
 const About = () => {
     return (
         <div data-aos="fade-up" className='aboutUs'>
@@ -15,11 +17,20 @@ const About = () => {
                 <div className='ratings'>
 
                     {
-                        Ratings.map(rating => {
+                        Ratings.map(rating => {            
                             return (
                                 <div data-aos='fade-up' className='rating'>
                                     <img data-aos='flip-right' src={rating.img} className='img-fluid card-img' />
-                                    <h4 data-aos='fade-left' className='text-uppercase'>{rating.percentage}</h4>
+                                    <CountUp start={0} end={rating.percentage}>
+                                            {({ countUpRef, start }) => (
+                                                <ScrollTrigger onEnter={start}>
+                                                    <div data-aos='fade-left' className='d-flex'>
+                                                    <h4 onEnter={start} ref={countUpRef}>{rating.percentage}</h4>
+                                                    <h4 className='ms-2'>{rating.catagory}</h4>
+                                                </div>
+                                                </ScrollTrigger>
+                                            )}
+                                        </CountUp>
                                     <h6 data-aos='fade-right' className='text-uppercase'>{rating.name}</h6>
                                 </div>
                             )
